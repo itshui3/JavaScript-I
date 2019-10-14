a7b9f2ca7c1278180e8431f3119155681e283d3e
Hui <jeffrey.x.hui@gmail.com>
2019-10-13 17:17:46 -0700

    completed objects portion

--- assignments/objects.js
+++ assignments/objects.js
@@ -77,7 +77,11 @@ kennan.speak = function() {
 console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
-//console.log(antonietta.multiplyNums(3,4));
+
+antonietta.multiplyNums = function(num1, num2) {
+  return num1*num2;
+}
+console.log(antonietta.multiplyNums(3,4));
 
 // === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
 
